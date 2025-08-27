76ef77f8085133b3f880d0d9a7825776a13c7a9a
7eduardocf <eduardocfrota901@gmail.com>
2025-08-26 23:17:45 -0400

    style(botão): iniciando a estilização do botão

--- src/components/Header/Nav/Link/index.tsx
+++ src/components/Header/Nav/Link/index.tsx
@@ -1,9 +1,10 @@
 import type { ILink } from "../../../../interfaces/ILink"
+import styles from "./Link.module.css"
 
 const Link = ({url, imagem, alt}: ILink) => {
   return (
-    <div>
-        <a href={url}>
+    <div className={styles.box}>
+        <a href={url} >
             <img src={imagem} alt={alt}/>
         </a>
     </div>
